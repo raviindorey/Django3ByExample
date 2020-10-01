cf9165da0508ba8a76b0f73e688f3d39091d935d
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-01 16:40:35 +0530

    Custom ORM manager

--- Chapter01/mysite/blog/models.py
+++ Chapter01/mysite/blog/models.py
@@ -3,21 +3,39 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).\
+            get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
     )
+
+    # Default manager
+    objects = models.Manager()
+    # Custom manager
+    published = PublishedManager()
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
-        User, related_name='blog_posts', on_delete=models.CASCADE)
+        User,
+        related_name='blog_posts',
+        on_delete=models.CASCADE
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default='draft')
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+    )
 
     class Meta:
         ordering = ('-publish',)
