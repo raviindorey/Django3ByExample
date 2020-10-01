af0803c21c201e4cb6ae4dd7058dd654e2ecaf3a
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-01 17:23:56 +0530

    Blog urls and absolute url

--- Chapter01/mysite/blog/models.py
+++ Chapter01/mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -36,6 +37,13 @@ class Post(models.Model):
         choices=STATUS_CHOICES,
         default='draft'
     )
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month,
+                  self.publish.day, self.slug]
+        )
 
     class Meta:
         ordering = ('-publish',)
