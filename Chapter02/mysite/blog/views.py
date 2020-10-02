b11901c7971634107b802cd107813c1c77503b1f
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-02 23:16:16 +0530

    Blog posts: discard class based view for post_list

--- Chapter02/mysite/blog/views.py
+++ Chapter02/mysite/blog/views.py
@@ -1,37 +1,30 @@
 from django.core.mail import send_mail
-from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {
-#             'posts': posts,  # a Paginator object
-#             'page': page,
-#         }
-#     )
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,  # a Paginator object
+            'page': page,
+        }
+    )
 
 
 def post_detail(request, year, month, day, post_slug):
