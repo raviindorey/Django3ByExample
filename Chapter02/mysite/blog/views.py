1257c7e3ca1e41d552df0e6120dddf170ee9d1f1
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-03 00:35:35 +0530

    Blog posts tags: Show post with similar tags

--- Chapter02/mysite/blog/views.py
+++ Chapter02/mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from taggit.models import Tag
 
 from .models import Post
@@ -52,11 +53,20 @@ def post_detail(request, year, month, day, post_slug):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List similar posts
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
         'comment_form': comment_form,
+        'similar_posts': similar_posts,
     })
 
 
