a49db7c3718d97fdafc24bc9724262d235b3ef75
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-02 20:10:15 +0530

    Bolg post comments: view

--- Chapter02/mysite/blog/views.py
+++ Chapter02/mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -39,7 +39,23 @@ def post_detail(request, year, month, day, post_slug):
         Post, slug=post_slug, status='published',
         publish__year=year, publish__month=month, publish__day=day
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+    })
 
 
 def post_share(request, post_id):
