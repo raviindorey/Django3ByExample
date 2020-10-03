3c8e41129b194ae71ecabd4b7e20f4d05e4fbcac
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-03 16:59:36 +0530

    RSS feed for posts

--- Chapter03/mysite/blog/urls.py
+++ Chapter03/mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, post_share
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post_slug>/',
          post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
