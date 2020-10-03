76d4e43d3550da318d4aee4a0a3cd540209e86b6
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-03 09:16:35 +0530

    Initialize from Chapter02

---
+++ Chapter03/mysite/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import post_list, post_detail, post_share
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post_slug>/',
+         post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
+]
