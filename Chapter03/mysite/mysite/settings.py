81f721dd598b5d3ace7094902864a3cd010c9b11
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-03 17:39:50 +0530

    Postgres setup

--- Chapter03/mysite/mysite/settings.py
+++ Chapter03/mysite/mysite/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
     }
 }
 
