e16a1b5117c30ad559bed92c57b9e39e389b739a
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-04 19:43:13 +0530

    New user registration

--- Chapter04/bookmarks/account/urls.py
+++ Chapter04/bookmarks/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import dashboard
+from .views import dashboard, register
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
 ]
