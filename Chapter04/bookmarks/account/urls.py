9ed68aa28cf8ea5cfd13cea4b9fc2d5795cb27a3
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-04 18:40:51 +0530

    Urls and Templates added for password change and reset: 1. Password change form. 2. Password change done. 3. Password reset form. 4. Password reset email. 5. Password reset done. 6. Password reset confirm. 7. Password reset complete.

--- Chapter04/bookmarks/account/urls.py
+++ Chapter04/bookmarks/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView,
+    PasswordChangeView, PasswordChangeDoneView,
+    PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView,
+)
 
 # from .views import user_login
 from .views import dashboard
@@ -8,5 +13,17 @@ urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
     path('', dashboard, name='dashboard'),
 ]
