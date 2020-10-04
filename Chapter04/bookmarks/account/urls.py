60f7b895eb4db6c132004b9950e90dadd8a9bb45
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-04 14:19:02 +0530

    Django's factory LoginView and LogoutView

--- Chapter04/bookmarks/account/urls.py
+++ Chapter04/bookmarks/account/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import user_login
+# from .views import user_login
+from .views import dashboard
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
--- Chapter04/bookmarks/bookmarks/settings.py
+++ Chapter04/bookmarks/bookmarks/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Customizations
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'dashboard'
