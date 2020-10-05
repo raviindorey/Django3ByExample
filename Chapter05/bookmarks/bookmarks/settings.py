d025be64b9511dbacb7b87148d57a482a67e6cb4
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-06 02:33:43 +0530

    Images app for saving images: models and admin

--- Chapter05/bookmarks/bookmarks/settings.py
+++ Chapter05/bookmarks/bookmarks/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Local
+    'images.apps.ImagesConfig',
+
     # 3rd party
     'social_django',
     'django_extensions' if DEBUG else None,
