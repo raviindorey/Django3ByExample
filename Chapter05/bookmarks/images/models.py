0fd0c6bdad3aba2c42392b15c6858d70dc962c90
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-07 20:03:26 +0530

    Images: show saved image detail page

--- Chapter05/bookmarks/images/models.py
+++ Chapter05/bookmarks/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.conf import settings
 from django.utils.text import slugify
 
@@ -28,3 +29,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
