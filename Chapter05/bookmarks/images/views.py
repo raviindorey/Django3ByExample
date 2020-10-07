9cf6ab17e3f928ba6a56155613793a2291a2c647
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-07 17:07:54 +0530

    Image app: view, templates and url to save image

--- Chapter05/bookmarks/images/views.py
+++ Chapter05/bookmarks/images/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(request.POST)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(request.GET)
+        return render(request, 'images/image/create.html', {
+            'section': 'images',
+            'form': form,
+        })
