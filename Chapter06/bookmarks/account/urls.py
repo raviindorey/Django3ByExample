a51af40cd9d970489c18e332c80033c7c8c789d7
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-08 17:48:04 +0530

    Show users: list and detail

--- Chapter06/bookmarks/account/urls.py
+++ Chapter06/bookmarks/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import dashboard, register, edit
+from .views import dashboard, register, edit, user_list, user_detail
 
 urlpatterns = [
     path('edit/', edit, name='edit'),
     path('register/', register, name='register'),
+    path('users/', user_list, name='user_list'),
+    path('users/<username>', user_detail, name='user_detail'),
     path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
 ]
--- Chapter06/bookmarks/account/views.py
+++ Chapter06/bookmarks/account/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -49,4 +50,24 @@ def edit(request):
     return render(request, 'account/edit.html', {
         'user_form': user_form,
         'profile_form': profile_form,
+    })
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html', {
+        'section': 'people',
+        'users': users,
+    })
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    is_following = request.user in user.followers.all()
+    return render(request, 'account/user/detail.html', {
+        'section': 'people',
+        'user': user,
+        'is_following': is_following,
     })
