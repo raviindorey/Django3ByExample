c4875b4f3a44255e28a847622b4d39adceaf0e74
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-08 18:16:19 +0530

    View to handle AJAX follow request

--- Chapter06/bookmarks/account/urls.py
+++ Chapter06/bookmarks/account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 
-from .views import dashboard, register, edit, user_list, user_detail
+from .views import (
+    dashboard, register, edit,
+    user_list, user_detail, user_follow,
+)
 
 urlpatterns = [
     path('edit/', edit, name='edit'),
     path('register/', register, name='register'),
     path('users/', user_list, name='user_list'),
+    path('users/follow/', user_follow, name='user_follow'),
     path('users/<username>', user_detail, name='user_detail'),
     path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
