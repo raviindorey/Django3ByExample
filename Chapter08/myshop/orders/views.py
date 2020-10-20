6452e1b7ff5ce791f7b329e69a2b23376a1ab59a
Ravi Indorey <ravi.indorey90@gmail.com>
2020-10-20 18:08:49 +0530

    Initialize from Chapter07

---
+++ Chapter08/myshop/orders/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+from .tasks import order_created
+from .forms import OrderCreateForm
+from .models import OrderItem
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity'],
+                )
+            cart.clear()
+            # async send email
+            order_created.delay(order.id)
+            return render(request, 'orders/order/created.html', {
+                'order': order,
+            })
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {
+        'cart': cart,
+        'form': form,
+    })
